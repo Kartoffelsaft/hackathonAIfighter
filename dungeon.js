3505442093ebc569033e58de0a2987de4334bcf9
jekaco <89284890+jekaco@users.noreply.github.com>
2024-04-28 00:37:03 -0700

    Add function to add buttons and scroll chatbox

--- dungeon.js
+++ dungeon.js
@@ -1,13 +1,28 @@
-let messages = document.getElementById("chatbox")
-scrollToBottom = () => {
-  messages.scrollTo(0, messages.scrollHeight);
-}
-
 function printChat(str1) {
-  let finalStr = '<div class="chatbox">' + str1  + '</div>';
+  let messages = document.getElementById("chatbox")
+  scrollToBottom = () => {
+    messages.scrollTo(0, messages.scrollHeight);
+  }
+
+  let finalStr = '<p class="chatbox">' + str1 + '</p>';
   messages.insertAdjacentHTML("afterbegin", finalStr);
+  scrollToBottom();
 }
 
-for(let index = 0; index < 10; index++) {
-  printChat(index);
+function addAbility(name, effect) {
+  let button = document.createElement('button');
+  button.innerText = name;
+  button.style.cssText = 'flex: 1 1;max-width: 100px;'
+  button.addEventListener('click', () => {
+    printChat(effect);
+
+    /* Add damage here */
+  })
+  document.getElementById("abilities").appendChild(button);
+}
+
+/* main */
+printChat("player is using " + gamestate.player.weapon.name + "!\n");
+for(let i = 0; i < gamestate.player.weapon.abilities.length; i++) {
+  addAbility(gamestate.player.weapon.abilities[i].name, gamestate.player.weapon.abilities[i].effect);
 }
